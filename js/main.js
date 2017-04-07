138b36aea2ab75b47f3e6a705302e6ddb10ec34e
Daniel Danielecki <danieldanielecki@daniels-macbook-pro.home>
2017-04-07 14:36:26 +0200

    add Shop

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ game.state.add("LoadingState", new CashNinja.LoadingState());
 game.state.add("GameState", new CashNinja.LevelState());
 game.state.add("TitleState", new CashNinja.TitleState());
 game.state.add("StoreState", new CashNinja.StoreState());
+game.state.add("ShopState", new CashNinja.ShopState());
 game.state.add("FirebaseState", new CashNinja.FirebaseState());
 game.state.start("BootState", true, false, "assets/levels/title_screen.json", "TitleState");
 // game.state.start("FirebaseState");
